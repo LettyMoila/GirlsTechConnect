0979b3fb7940c8670e5e6f3d4eeb08c8b3e6501e
LettyMoila <moilankibelo@gmail.com>
2023-08-15 02:55:34 -0700

    completed forecast

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import rain from './assets/icons/rain.png'
 import sunny from './assets/icons/sun.png'
+import star from './assets/icons/star.png'
 import sunCloudy from './assets/icons/sunCloudy.png'
 import thunder from './assets/icons/rainyThunder.png'
 
 function App() {
-  // const [lat, setLat] = useState([]);
-  // const [long, setLong] = useState([]);
   const [data, setData] = useState();
-
-  // console.log(import.meta.env.VITE_API_URL)
 
 
   useEffect(() => {
@@ -24,7 +21,7 @@ function App() {
         long = position.coords.longitude
         lat = position.coords.latitude
 
-        fetch(`${import.meta.env.VITE_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${import.meta.env.VITE_API_KEY}`)
+        fetch(`${import.meta.env.VITE_API_URL}/forecast/?lat=${lat}&lon=${long}&units=metric&APPID=${import.meta.env.VITE_API_KEY}`)
         .then(res => res.json())
         .then(result => {
           setData(result);
@@ -40,54 +37,99 @@ function App() {
   console.log(data)
 
   return (
-    <div className='h-screen  bg-[url("./assets/summer.jpg")] bg-cover bg-no-repeat p-6'>
-      {data ? (
-     <div className='overall h-full w-full'>
-      <section className='h-2/4 bg-blue2 rounded-3xl opacity-60 flex'>
+    
+  
+     <div className='h-screen  bg-[url("./assets/summer.jpg")] bg-cover bg-no-repeat p-6'>
+       {data ? (
+      <div className='overall h-full w-full'>
+       <section className='h-2/4 bg-blue2 rounded-3xl opacity-60 flex'>
      
         
-          <section className='flex w-full'>
-            <div className='w-2/4 p-2'>
-              <h2> {data.name}</h2>
-              <img src={sunny} className='h-32 w-32 translate-x-72'/>
-              <p className='text-4xl'>{data.main.temp}°C</p>
-              <p> {data.weather[0].description}</p>
-            </div>
-            <div className='w-2/4 p-2 grid grid-cols-2 '>
-              <div>humidity <br></br> {data.main.humidity}%</div>
-              <div>max temp <br/>{data.main.temp_max}°C</div>
-              <div>min temp <br/> {data.main.temp_min}°C</div>
-              <div>wind speed <br/> {data.wind.speed}km/h</div>
-              <div>pressure <br/>{data.main.pressure} mb</div>
-              <div>rain <br/> %</div>
-            </div>
-          </section>
+           <section className='flex w-full'>
+             <div className='w-2/4 p-2 grid justify-center'>
+               <h2> {data.city.name}</h2>
+               <img src={sunny} className='h-32 w-32'/>
+               <p className='text-4xl'>{data.list[0].main.temp}°C</p>
+               <p> {data.list[0].weather[0].description}</p>
+             </div>
+             <div className='w-2/4 p-2 grid grid-cols-2 '>
+               <div>humidity <br></br> {data.list[0].main.humidity}%</div>
+               <div>max temp <br/>{data.list[0].main.temp_max}°C</div>
+               <div>min temp <br/> {data.list[0].main.temp_min}°C</div>
+               <div>wind speed <br/> {data.list[0].wind.speed}km/h</div>
+               <div>pressure <br/>{data.list[0].main.pressure} mb</div>
+               <div>rain <br/> %</div>
+             </div>
+           </section>
           
            
       
-      </section>
-      <section className='h-2/4 pt-3'>
-        <div className='flex justify-between'>
-        <h1 className='font-bold text-left'>Weekly</h1>
-        <form className=' h-10'>
-          <input className='bg-none bg-blue3 rounded-2xl opacity-60' placeholder='search location'/>
+       </section>
+       <section className='h-2/4 pt-3'>
+         <div className='flex justify-between'>
+         <h1 className='font-bold text-left'>Forecast</h1>
+         <form className=' h-10'>
+           <input className='bg-none bg-blue3 rounded-2xl opacity-60' placeholder='search location'/>
 
-        </form>
-        </div>
-        <section className='flex h-60 p-4'>
-          <div className='bg-blue3 h-full w-36 rounded-3xl opacity-60 grid'>
-            <section>{new Date(data.dt * 1000).toLocaleDateString()}</section>
-            <section></section>
-            <section>{data.main.temp}°C</section>
-          </div>
-        </section>
-      </section>
-      </div>
-      ) : (
-        <p>Loading weather data...</p>
+         </form>
+         </div>
+         <section className='flex gap-2 h-60 p-4'>
+           <div className='bg-blue3 h-full w-36 rounded-3xl opacity-60 grid'>
+            {/*data.list.map(days, index)=>(
+              <p key={index}>{days}</p>
+            )*/}
+             <section>{data.list[1].dt_txt}</section>
+             <section className='grid justify-center'><img src={sunny} className='h-24 w-24'/></section>
+             <section>{data.list[1].main.temp}°C</section>
+           </div>
+           <div className='bg-blue3 h-full w-36 rounded-3xl opacity-60 grid'>
+             <section>{data.list[2].dt_txt}</section>
+             <section className='grid justify-center'><img src={sunny} className='h-24 w-24'/></section>
+             <section>{data.list[2].main.temp}°C</section>
+           </div>
+           <div className='bg-blue3 h-full w-36 rounded-3xl opacity-60 grid'>
+             <section>{data.list[3].dt_txt}</section>
+             <section className='grid justify-center'><img src={star} className='h-24 w-24'/></section>
+             <section>{data.list[3].main.temp}°C</section>
+           </div>
+           <div className='bg-blue3 h-full w-36 rounded-3xl opacity-60 grid'>
+             <section>{data.list[4].dt_txt}</section>
+             <section className='grid justify-center'><img src={star} className='h-24 w-24'/></section>
+             <section>{data.list[4].main.temp}°C</section>
+           </div>
+           <div className='bg-blue3 h-full w-36 rounded-3xl opacity-60 grid'>
+             <section>{data.list[5].dt_txt}</section>
+             <section className='grid justify-center'><img src={star} className='h-24 w-24'/></section>
+             <section>{data.list[5].main.temp}°C</section>
+           </div>
+           <div className='bg-blue3 h-full w-36 rounded-3xl opacity-60 grid'>
+             <section>{data.list[6].dt_txt}</section>
+             <section className='grid justify-center'><img src={sunny} className='h-24 w-24'/></section>
+             <section>{data.list[6].main.temp}°C</section>
+           </div>
+           <div className='bg-blue3 h-full w-36 rounded-3xl opacity-60 grid'>
+             <section>{data.list[7].dt_txt}</section>
+             <section className='grid justify-center'><img src={sunny} className='h-24 w-24'/></section>
+             <section>{data.list[7].main.temp}°C</section>
+           </div>
+           <div className='bg-blue3 h-full w-36 rounded-3xl opacity-60 grid'>
+             <section>{data.list[8].dt_txt}</section>
+             <section className='grid justify-center'><img src={sunny} className='h-24 w-24'/></section>
+             <section>{data.list[8].main.temp}°C</section>
+           </div>
+           <div className='bg-blue3 h-full w-36 rounded-3xl opacity-60 grid'>
+             <section>{data.list[9].dt_txt}</section>
+             <section className='grid justify-center'><img src={sunny} className='h-24 w-24'/></section>
+             <section>{data.list[9].main.temp}°C</section>
+           </div>
+         </section>
+       </section>
+       </div>
+       ) : (
+         <p>Loading weather data...</p>
       )}
-    </div>
-  )
+     </div>
+   )
 }
 
 export default App
